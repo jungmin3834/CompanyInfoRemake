109bfc2b51f2d85cc4b424c6cced180ffcea6d8b
sbj3834 <sbj3834@naver.com>
2020-02-01 12:07:31 +0100

    Node express module + user.json file add

--- Server(Nodejs)/NodeServerFile/test.js
+++ Server(Nodejs)/NodeServerFile/test.js
@@ -6,3 +6,26 @@ http.createServer(function (req, res) {
 ).listen(3333, '127.0.0.1'); 
 
 console.log('Server running at http://127.0.0.1:3333/');
+
+
+module.exports = function(app, fs)
+{
+
+     app.get('/',function(req,res){
+         res.render('index', {
+             title: "MY HOMEPAGE",
+             length: 5
+         })
+     });
+
+
+
+    app.get('/list', function (req, res) {
+       fs.readFile( __dirname + "/../data/" + "user.json", 'utf8', function (err, data) {
+           console.log( data );
+           res.end( data );
+       });
+    })
+
+
+}
