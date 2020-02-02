8ec39cc1e92f60f096dd88b46d92b524ad3516db
sbj3834 <sbj3834@naver.com>
2020-02-02 14:57:11 +0100

    Post 메세지 Get메세지 방식 구현 방법 및 테스트

--- Client(Python)/Main.py
+++ Client(Python)/Main.py
@@ -1,20 +1,27 @@
 from Window.Ui_MainWindow import Ui_StartWindow
 
 import requests
+import json
 
+url = "http://localhost:3000"
+data = {'head' : 'Junmin' , 'msg': 'Hi!!!'}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
 
 def send_message_node(url):
-    r = requests.get(url)
+    params = {'param1': 'jungmin'}
+    r = requests.get(url , params= params)
+
     print(r.text)
     return r
 
 
 def get_host_path(request_type):
-    url = "http://localhost:3000/"
+    url = "http://localhost:3000/list"
     return url + request_type
 
 
-send_message_node(get_host_path("hello"))
+#send_message_node(get_host_path(""))
 
 
 def MainStart():
--- Server(Nodejs)/NodeServerFile/test.js
+++ Server(Nodejs)/NodeServerFile/test.js
@@ -1,18 +1,36 @@
 //express 라이브러리를 가지고 옴 안될 경우 npm install express
 var express = require('express'); 
 
-//express 변수 안에 있는 속성값을 사용하기 위해 app에 저장
+
+var bodyParser = require('body-parser');
+
 var app = express(); 
+
+app.use(bodyParser.json())
+app.post('/',function(req,res){
+        var msg = req.body.msg;
+        
+        console.log("python: " + req.body.msg + " : " + req.body.head);
+});
+
+//express 변수 안에 있는 속성값을 사용하기 위해 app에 저장
+
+
+app.get(function(res)
+{
+    console.log("여기 ! " + res)
+})
 
 //get 함수 등록 첫번째 경로에 엑세스 되었을 때 두번째 인자의 함수 호출
 app.get('/', function(req, res){ 
-    console.log(req);
-    console.log(res);
+    console.log("여깅"+ req.url  + "\n"+ req.query["param1"]);
+    
     res.send('Hello World!'); 
 }) 
 
 //test용 함수로 /list 경로에 접근하게 되었을 때 발생
 app.get('/list', function(req, res){ 
+    console.log("list \n"+ req.url  + "\n"+ req.query["param1"]);
     res.send('list Test Success'); 
 }) 
 
